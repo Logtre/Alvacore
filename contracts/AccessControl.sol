1017f8a0ee839b20acb1da137108f7c05511b618
Gucci <bokuricci@gmail.com>
2018-11-16 15:48:22 +0900

    Add comments.

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -1,37 +1,57 @@
 pragma solidity ^0.4.24;
 
 
+/**
+    AccessControl define contract owner's authority
+*/
 contract AccessControl {
 
     address internal owner;
 
     bool public paused = false;
-
+    /**
+        @dev onlyOwner
+    */
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
+    /**
+        @dev whenNotPaused revert tx. If paused flag is effective.
+    */
     modifier whenNotPaused() {
         require(!paused);
         _;
     }
 
+    /**
+        @dev whenPaused revert tx. If paused flag is not effective.
+    */
     modifier whenPaused() {
         require(paused);
         _;
     }
 
+    /**
+        @dev adminSetOwner change contract's owner.
+    */
     function adminSetOwner(address _newOwner) external onlyOwner() {
         require(_newOwner != address(0));
 
         owner = _newOwner;
     }
 
+    /**
+        @dev adminPause set pause flag.
+    */
     function adminPause() external onlyOwner() whenNotPaused() {
         paused = true;
     }
 
+    /**
+        @dev adminUnpause release pause flag.
+    */
     function adminUnpause() public onlyOwner() whenPaused() {
         paused = false;
     }
--- contracts/Base.sol
+++ contracts/Base.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.4.24;
 import "https://github.com/Logtre/Alvacore/contracts/AccessControl.sol";
 
 
+/**
+    Base define common function
+*/
 contract Base is AccessControl {
 
     event Upgrade(address newAddr);
@@ -28,21 +31,33 @@ contract Base is AccessControl {
         _;
     }
 
+    /**
+        @dev available revert tx, if killswitch flag is set, and newVersion flag is set.
+    */
     modifier available() {
         require(killswitch == false && newVersion == 0);
         _;
     }
 
+    /**
+        @dev noKillswitch revert tx, if killswitch flag is set.
+    */
     modifier noKillswitch() {
         require(killswitch == false);
         _;
     }
 
+    /**
+        @dev noCancelFlag revert tx, if cancelFlag is set.
+    */
     modifier noCancelFlag() {
         require(cancelFlag == false);
         _;
     }
 
+    /**
+        @dev noNewVersion revert tx, if newVersion flag is set.
+    */
     modifier noNewVersion() {
         require(newVersion == 0);
         _;
@@ -55,6 +70,9 @@ contract Base is AccessControl {
         newVersion = 0;
     }
 
+    /**
+        @dev _convertBytesToBytes8 change from multi-bytes text data to 8byte text data.
+    */
     function _convertBytesToBytes8(bytes inBytes) internal pure returns (bytes8 outBytes8) {
         if (inBytes.length == 0) {
             return 0x0;
@@ -65,6 +83,9 @@ contract Base is AccessControl {
         }
     }
 
+    /**
+        @dev _convertBytesToBytes32 change from multi-bytes text data to 32byte text data.
+    */
     function _convertBytesToBytes32(bytes inBytes) internal pure returns (bytes32 outBytes32) {
         if (inBytes.length == 0) {
             return 0x0;
@@ -75,24 +96,40 @@ contract Base is AccessControl {
         }
     }
 
+    /**
+        @dev _withdraw send all asset in contract to owner's address.
+    */
     function _withdraw() internal {
         if (!owner.call.value(address(this).balance)()) {
             revert();
         }
     }
 
+    /**
+        @dev _upgrade change contract's version.
+    */
     function _upgrade(address _newAddr) internal {
         newVersion = -int(_newAddr);
         killswitch = true;
         emit Upgrade(_newAddr);
     }
 
+    /**
+        @dev _restart change newVersion flag.
+    */
     function _restart() internal {
         if (newVersion == 0) {
             killswitch = false;
         }
     }
 
+    /**
+        @dev _setFees set contract's fees.
+        @param _gasPrice: gasprice which is set when user create transactions.
+        @param _minGas: minimum gasprice which is set when user create transactions.
+        @param _cancellationGas: gasprice which is needed when cancel order.
+        @param _txGasPrice: gasprice which is needed when order to contract.
+    */
     function _setFees(int _gasPrice, int _minGas, int _cancellationGas, int _externalGas, int _txGasPrice) internal {
         gasPrice = _gasPrice;
         minGas = _minGas;
@@ -100,23 +137,44 @@ contract Base is AccessControl {
         externalGas = _externalGas;
         txGasPrice = _txGasPrice;
     }
+
+    /**
+        @dev _resetKillswitch release killswitch flag.
+    */
     function _resetKillswitch() internal {
         killswitch = false;
     }
 
+    /**
+        @dev _resetUnrespond release unrespondedCnt.
+    */
     function _resetUnrespond() internal {
         unrespondedCnt = 0;
     }
 
+    /**
+        @dev _setNewVersion set newVersion flag.
+    */
     function _setNewVersion(address _newAddr) internal {
         newVersion = int(_newAddr);
     }
 
+    /**
+        @dev _setAllCancel set cancelFlag.
+    */
+    function _setAllCancel(bool _flag) internal {
+        cancelFlag = _flag;
+    }
+}
+
+    /**
+        @dev _externalCall send contract's asset to external address by call method
+        if transfer volume greater than balance,
+        set volume as 80% of balance.
+        if there is surplus asset, administor
+        withdraw via withdraw function.
+    */
     function _externalCall(address _to, int _value) internal {
-        // if transfer volume greater than balance,
-        // set volume as 80% of balance.
-        // if there is surplus asset, administor
-        // withdraw via withdraw function.
         if (_value > int(address(this).balance)) {
             _value = int(address(this).balance * 80 / 100);
         }
@@ -126,11 +184,14 @@ contract Base is AccessControl {
         }
     }
 
+    /**
+        @dev _transfer send contract's asset to external address by send function
+        if transfer volume greater than balance,
+        set volume as 80% of balance.
+        if there is surplus asset, administor
+        withdraw via withdraw function.
+    */
     function _transfer(address _to, int _value) internal {
-        // if transfer volume greater than balance,
-        // set volume as 80% of balance.
-        // if there is surplus asset, administor
-        // withdraw via withdraw function.
         if (_value > int(address(this).balance)) {
             _value = int(address(this).balance * 80 / 100);
         }
@@ -139,8 +200,3 @@ contract Base is AccessControl {
             revert();
         }
     }
-
-    function _setAllCancel(bool _flag) internal {
-        cancelFlag = _flag;
-    }
-}
